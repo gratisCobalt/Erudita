02e5e621ec5a3cec64067127177027e224479bf9
gratisCobalt <jaywatson@gmx.net>
2023-01-16 21:27:12 +0100

    added footer

---
+++ app/components/footer.php
@@ -0,0 +1,25 @@
+<footer class="bg-dark py-5 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 text-center text-md-left">
+                <div class="text-white">
+                    <img src="assets/v2_round.png" alt="Logo" class="img-fluid mb-3" width="32" height="32">
+                </div>
+                <p class="text-muted mb-0">Copyright &copy; <?php echo date('Y'); ?></p>
+            </div>
+            <div class="col-12 col-md-8 text-center text-md-right">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item mx-3">
+                        <a href="components/Datenschutzerklärung.html" class="text-muted">Datenschutz</a>
+                    </li>
+                    <li class="list-inline-item mx-3">
+                        <a href="components/Impressum.html" class="text-muted">Impressum</a>
+                    </li>
+                    <li class="list-inline-item mx-3">
+                        <a href="faq.php" class="text-muted">FAQ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
