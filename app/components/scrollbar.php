4a4e1335c458a35ab927424776871d7a794a5961
gratisCobalt <jaywatson@gmx.net>
2023-01-16 21:28:05 +0100

    added scrollbar modification

---
+++ app/components/scrollbar.php
@@ -0,0 +1,11 @@
+<style>
+    ::-webkit-scrollbar {
+        width: 12px;
+        background-color: #F5F5F5;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+        background-color: #555;
+    }
+</style>
