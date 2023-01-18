8500f31de565fae240889431b0ec287bda85afa0
gratisCobalt <jaywatson@gmx.net>
2023-01-18 14:54:57 +0100

    moved all credentials to credentials file

--- app/config.php
+++ app/config.php
@@ -3,12 +3,12 @@
 require_once('credentials.php');
 
 // Database Credentials
-//! please check if your credentials are the same
-define('DB_HOST', 'manny.db.elephantsql.com'); // localhost
-define('DB_PORT', 5432); // 5432
-define('DB_NAME', 'xdncrrtq'); // Erudita
-define('DB_USER', 'xdncrrtq'); // postgres
-define('DB_PASS', $DB_PASS); // add your database password here
+//TODO: please create a 'credentials.php' file and add your credentials
+define('DB_HOST', $DB_HOST);
+define('DB_PORT', $DB_PORT);
+define('DB_NAME', $DB_NAME);
+define('DB_USER', $DB_USER);
+define('DB_PASS', $DB_PASS);
 
 try {
   $pdo = new PDO("pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
@@ -17,6 +17,7 @@ try {
 }
 
 // Google reCAPTCHA
-$site_key = '';
+//TODO: please create your site key here
+$site_key = '6Lc_dwkkAAAAAF2kAxNHhhTU_quWVPM4C214vEiB';
 
 ?>
