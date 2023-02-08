c0be149886292eb66c04c247cb7ea5f104c6c6b2
gratisCobalt <jaywatson@gmx.net>
2023-02-08 13:06:46 +0100

    moved chat box to bottom

--- app/chat.php
+++ app/chat.php
@@ -33,22 +33,22 @@ sendMessages($author_id, $msgContent);
                 <p class="lead mb-4">Introducing our new chat feature, a real-time communication tool designed to enhance collaboration and productivity among our team. Start using the chat feature today and experience the power of connected teamwork.
             </div>
         </div>
-        <div class="container" style="width: 30rem; clear: both;">
+        <br>
+        <div class="container" style="height: 30rem; width: 40rem; overflow-y: scroll; clear: both;" id="chatbox">
+          <?php foreach ($messages as $message):
+          $senderId = $message['author_id'];
+          $sender = getUserByID($senderId);
+          ?>
+        <p><strong><?php echo $sender['username'];?>:</strong> <span class="text-muted"><?php echo $message['content'];?></span></p>
+        <?php endforeach; ?>
+      </div>
+      <div class="container my-3" style="width: 30rem; clear: both;">
         <form class="col-sm-18" method="post">
           <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Write a message" aria-label="Message" aria-describedby="msgSend" name="msgContent" id="msgContent">
+            <input type="text" class="form-control mx-3" placeholder="Write a message" aria-label="Message" aria-describedby="msgSend" name="msgContent" id="msgContent">
             <input class="btn btn-outline-dark" type="submit" name="msgSend" id="msgSend" value="Send">
           </div>
         </form>
-      </div>
-      <br>
-      <div class="container" style="height: 30rem; width: 40rem; overflow-y: scroll; clear: both;" id="chatbox">
-      <?php foreach ($messages as $message):
-          $senderId = $message['author_id'];
-          $sender = getUserByID($senderId);
-        ?>
-        <p><strong><?php echo $sender['username'];?>:</strong> <span class="text-muted"><?php echo $message['content'];?></span></p>
-      <?php endforeach; ?>
     </div>
     </div>
 </body>
